1c3c317963ba263a2c8b885b81aff28f8d60a3c3
PanagiotisEvaggelou <80092725+PanagiotisEvaggelou@users.noreply.github.com>
2024-03-04 00:52:07 +0200

    Update tic-tac-toe-game.py

--- tic-tac-toe-game.py
+++ tic-tac-toe-game.py
@@ -1,33 +1,3 @@
-'''
-Kanones tou paixnidiou:
-- Skopos toy paixnidiou einai o sxhmatismos oso perissoterwn tetradwn apo ta pionia tou kaue paikth.
-
-- Enas gyros oloklhrvnetai otan kapoios apo tous dyo paiktes sxhmatisei me ta pionia toy mia tetrada
-  orizontia, kaueta h diagwnia.
-
-- Me ton sxhmatismo tetradas apo opoiondhopote paikth, prosthuetai enas pontos stis nikes tou sygkekrimenou
-  paikth kai xarakthrizetai nikhths tou gyrou.
-
-- To paixnidi oloklhrwnetai otan meta apo thn nikh enos paikth kai thn oloklhrwsh tou gyroy, oi paiktes
-  epilejoyn oti den ueloyn na paijoyn ki allo gyro, h otan kata thn diarkeia enos gyrou to tamplo gemisei.
-
-- An to paixnidi teleiwsei, emfanizetai to teliko skor(o arithmos nikwn toy kaue paikth) kai o 
-  nikhths (o paikths me tous perissoterous nikhmeoys gyrous)
-
-Dieykriniseis:
-- An thn idia stigmh yparxoyn parapanw apo 4 pionia enos paikth sthn idia dieyuynsh (orizontia, kaueta, diagwnia)
-  tote afairoyntai mono 4 pionia, kai o paikths pairnei 1 ponto (mia nikh dhladh).
-
-- Otan afairethoyn ta pionia ths nikhthrias tetradas, kai ta pionia ths kaue sthlhs pesoun mia uesh pros ta katw, 
-  opoiadhpote allh tetrada dhmioyrghuei me ta pionia toy 1ou h tou 2ou paikth, ua prosteuei mia nikh sto skor toy antistoixou paikth
-
-- Otan oi paiktes epilejoun na apouhkeysoun thn  katastash toy paixnidiou se ena arxeio, san onoma tou arxeiou einai apodekto
-  opoiodhpote onoma (xwris ton typo tou arxeiou, px 'onoma1' kai oxi 'onoma1.txt' h 'onoma1.csv')
-'''
-
-#============================== Functions =============================================================================================
-
-
 def cr_table(value2):
     AZ=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 
@@ -695,10 +665,6 @@ def rounds(wins1,wins2):
             
 
             num=num+1
-
-
-#============================== Main =============================================================================================
-
 
 print('Καλωσήλθατε στο παιχνίδι!')
 value1 = input('Επιθυμείτε νέο παιχνίδι (Ν) ή φόρτωση παιχνιδιού από αρχείο (S); ')
